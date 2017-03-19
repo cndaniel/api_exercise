b37e72471a68d3d571594261ce03d8d776e8a544
cndaniel <cndaniel@me.com>
2017-03-19 18:26:07 +0100

    add api key

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -1,6 +1,21 @@
 class Api::V1::ReservationsController < ApiController
   before_action :set_up_reservation, only: [:show, :update]
+  before_action :authenticate_user!, :only => [:index]
 
+  def index
+    @reservations = current_user.
+    render json: {
+      data: @reservations.map{ |reservation|{
+      booking_code: reservation.booking_code,
+      train_number: reservation.train_number,
+      seat_number: reservation.seat_number,
+      customer_name: reservation.customer_name,
+      customer_phone: reservation.customer_phone
+    }
+    }
+  }
+
+  end
   def create
     @train = Train.find_by_number!( params[:train_number])
     @reservation = Reservation.new( train_id: @train.id,
@@ -8,7 +23,7 @@ class Api::V1::ReservationsController < ApiController
     customer_name: params[:customer_name],
     customer_phone: params[:customer_phone]
     )
-
+    @reservation.user = current_user
     if @reservation.save
       render json:{ booking_code: @reservation.booking_code,
         reservation_url: api_v1_reservation_url(@reservation.booking_code)
--- app/controllers/api/v1/trains_controller.rb
+++ app/controllers/api/v1/trains_controller.rb
@@ -2,20 +2,19 @@ class Api::V1::TrainsController < ApiController
   def index
     @trains = Train.all
     render json: {
-      data: @trains.map{ |train|
+      data: @trains.map do |train|
         { number: train.number,
-          train_url: api_v1_train_url(train.number)
-        }
-      }
+          train_url: api_v1_train_url(train.number) }
+      end
     }
   end
 
   def show
-    @train = Train.find_by_number!( params[:train_number ])
+    @train = Train.find_by_number!(params[:train_number])
     render json: {
       number: @train.number,
       available_seats: @train.available_seats
     }
-    #code
+    # code
   end
 end
